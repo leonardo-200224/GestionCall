f6ec3fcef2cac9658775645327207d8470b7d541
leonardo-20024 <ayalaperezleonardo24@gmail.com>
2026-05-04 04:23:45 -0500

    estadisticas

--- static/script.js
+++ static/script.js
@@ -339,3 +339,23 @@ function actualizarGraficaModal(clientes) {
         }
     });
 }
+
+// MENU SIDEBAR
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.querySelector(".sidebar");
+
+menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+});
+
+document.querySelectorAll(".sidebar a").forEach(link => {
+    link.addEventListener("click", () => {
+        sidebar.classList.remove("active");
+    });
+});
+
+document.addEventListener("click", (e) => {
+    if (!sidebar.contains(e.target) && !menuBtn.contains(e.target)) {
+        sidebar.classList.remove("active");
+    }
+});
